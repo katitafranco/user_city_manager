51cd09b3a9e9b427827c7c96527da827b4bd982e
Katiuska Franco <kati.franco@gmail.com>
2025-12-15 15:25:23 -0500

    feat(core): improve state management, routing, and bindings with GetX

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: constant_identifier_names
+
+part of './app_pages.dart';
+
+abstract class AppRoutes {
+  static const ROOT = '/';
+
+  /// *** Welcome path ***
+  static const InitPage = '/InitPage';
+
+  /// *** others path ***
+  /// *** Ciudades ***
+  static const CitiesPage = '/CitiesPage';
+}
